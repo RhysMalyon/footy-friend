97f24f86a9c71138cccc4e4f98b8eba701dfccd4
Rhys Malyon <malyonr@gmail.com>
2021-08-14 17:11:10 +0900

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# League > Stadium > Team > Player
+POSITION = %w[GK CB LB RB CDM CM CAM RW LW RM LM CF ST].freeze
+LEAGUES = ["Champion League", "Diamond League", "Platinum League", "Gold League", "Silver League", "Bronze League"]
+STADIA = ["Rhythm Park",
+          "Element Stadium",
+          "Moonlight Park",
+          "Beacon Centre",
+          "Onyx Stadium",
+          "Burrow Field",
+          "Bell Centre",
+          "Gemstone Arena",
+          "Vertex Ring",
+          "Sapphire Field",
+          "Phenomenon Bowl",
+          "Omen Arena",
+          "Halo Ground",
+          "Harmony Ground",
+          "Anomaly Centre",
+          "Alabaster Ring",
+          "Sanctum Bowl",
+          "Mirage Stadium",
+          "Aureole Stadium",
+          "Pride Field"]
+
+puts "Clearing leagues..."
+League.destroy_all
+
+puts "Populating league database..."
+LEAGUES.each do |league|
+  League.create!(
+    name: league
+  )
+end
+puts "Leagues populated!"
+
+puts "Clearing stadia..."
+Stadium.destroy_all
+
+puts "Populating stadium database..."
+STADIA.each do |stadium|
+  Stadium.create!(
+    name: stadium,
+    capacity: rand(10000..60000),
+    built: rand(1870..2010)
+  )
+end
+puts "Stadia populated!"
+
+
+puts "Populating team database..."
+Stadium.find_each do |stadium|
+  Team.create!(
+    name: Faker::Sports::Football.unique.team,
+    founded: rand(1880..2010),
+    city: Faker::Nation.capital_city,
+    league: League.all.sample,
+    stadium: stadium
+  )
+end
+puts "Teams populated!"
+
+puts "Populating team database..."
+Team.find_each do |team|
+  11.times do
+    Player.create!(
+      first_name: Faker::Name.male_first_name,
+      last_name: Faker::Name.last_name ,
+      age: rand(14..45),
+      nationality: Faker::Nation.nationality,
+      position: POSITION.sample,
+      team: team
+    )
+  end
+end
+puts "Teams populated!"
