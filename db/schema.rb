b089eba46ed0ab1c411de50938e0dd71bb6bb0c5
Rhys Malyon <malyonr@gmail.com>
2021-09-05 11:27:44 +0900

    Generate devise user model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_15_115624) do
+ActiveRecord::Schema.define(version: 2021_09_05_022638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,18 @@ ActiveRecord::Schema.define(version: 2021_08_15_115624) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["league_id"], name: "index_teams_on_league_id"
     t.index ["stadium_id"], name: "index_teams_on_stadium_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
