0a5e8a235776e304fed155b692a4210ac4dc342b
Rhys Malyon <malyonr@gmail.com>
2021-09-05 10:53:11 +0900

    Add edit and update routes to all models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :leagues, only: %i[index create new show destroy]
-  resources :teams, only: %i[index create new show destroy]
-  resources :players, only: %i[create new destroy]
-  resources :stadiums, only: %i[index create new show destroy]
+  resources :leagues
+  resources :players, only: %i[create new destroy edit update]
+  resources :stadiums
+  resources :teams
 end
